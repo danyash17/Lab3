d60a0652cd4b5402ee77232300d8847646594ab1
Daniil <danikshkabara@tut.by>
2020-11-14 16:47:09 +0300

    3th column fixed

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,5 +1,7 @@
 package controllers;
 import java.io.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -15,34 +17,82 @@ import javafx.stage.FileChooser;
 public class Controller {
     public TextArea t00;
     public TextArea t10;
-    public TextArea t20;
-    public TextArea t30;
-    public TextArea t40;
-    public TextArea t50;
-    public TextArea t60;
-    public TextArea t70;
-    public TextArea t80;
-    public TextArea t90;
     public TextArea t01;
     public TextArea t11;
-    public TextArea t21;
-    public TextArea t31;
-    public TextArea t41;
-    public TextArea t61;
-    public TextArea t51;
-    public TextArea t71;
-    public TextArea t81;
-    public TextArea t91;
+    public Pane p9;
     public TextArea t02;
+    public TextArea t03;
+    public TextArea t04;
+    public TextArea t05;
+    public TextArea t06;
+    public TextArea t07;
+    public TextArea t08;
+    public TextArea t09;
     public TextArea t12;
-    public TextArea t22;
-    public TextArea t32;
-    public TextArea t42;
-    public TextArea t52;
-    public TextArea t62;
-    public TextArea t72;
-    public TextArea t82;
-    public TextArea t92;
+    public TextArea t13;
+    public TextArea t14;
+    public TextArea t15;
+    public TextArea t16;
+    public TextArea t17;
+    public TextArea t18;
+    public TextArea t19;
+
+    @FXML
+    private Pane p0;
+
+    @FXML
+    private Label l0;
+
+    @FXML
+    private Pane p1;
+
+    @FXML
+    private Label l1;
+
+    @FXML
+    private Label l2;
+
+    @FXML
+    private Pane p2;
+
+    @FXML
+    private Label l3;
+
+    @FXML
+    private Pane p3;
+
+    @FXML
+    private Label l4;
+
+    @FXML
+    private Pane p4;
+
+    @FXML
+    private Label l5;
+
+    @FXML
+    private Pane p5;
+
+    @FXML
+    private Label l6;
+
+    @FXML
+    private Pane p6;
+
+    @FXML
+    private Label l7;
+
+    @FXML
+    private Pane p7;
+
+    @FXML
+    private Label l8;
+
+    @FXML
+    private Pane p8;
+
+    @FXML
+    private Label l9;
     private double from,to,step;
 private double[] a={5,5,1,0,-11};
     @FXML
@@ -92,12 +142,21 @@ private double[] a={5,5,1,0,-11};
         String key=Character.toString(last);
         return temp.startsWith(key);
     }
+    private boolean isSumTen(String num){
+        int sum=0;
+        for(int i=0;i<num.length();i++){
+            if(num.charAt(i)=='.') continue;
+            char temp=num.charAt(i);
+            int intedchar = Character.getNumericValue(temp);
+            sum+=intedchar;
+        }
+        return sum==10;
+    }
     @FXML
 private void doGorner(){
         String from_s="0";
         String to_s="0";
         String step_s="0";
-        String nums="1234567890";
         try{
             from_s=XFromField.getText();
             to_s=XToField.getText();
@@ -128,36 +187,117 @@ private void doGorner(){
                 res[j][i]=Double.toString(t[j][i]);
             }
         }
+        System.out.println(isSumTen(res[0][1]));
+        System.out.println(isSumTen(res[1][1]));
+        System.out.println(isSumTen(res[2][1]));
+        System.out.println(isSumTen(res[3][1]));
+        System.out.println(isSumTen(res[4][1]));
+        System.out.println(isSumTen(res[5][1]));
+        System.out.println(isSumTen(res[6][1]));
+        System.out.println(isSumTen(res[7][1]));
+        System.out.println(isSumTen(res[8][1]));
+        System.out.println(isSumTen(res[9][1]));
+
         t00.setText(res[0][0]);
-        if(isReflected(t[0][1])) t02.setText("TRUE");
-        t10.setText(res[1][0]);
-        if(isReflected(t[1][1])) t12.setText("TRUE");
-        t20.setText(res[2][0]);
-        if(isReflected(t[2][1])) t22.setText("TRUE");
-        t30.setText(res[3][0]);
-        if(isReflected(t[3][1])) t32.setText("TRUE");
-        t40.setText(res[4][0]);
-        if(isReflected(t[4][1])) t42.setText("TRUE");
-        t50.setText(res[5][0]);
-        if(isReflected(t[5][1])) t52.setText("TRUE");
-        t60.setText(res[6][0]);
-        if(isReflected(t[6][1])) t62.setText("TRUE");
-        t70.setText(res[7][0]);
-        if(isReflected(t[7][1])) t72.setText("TRUE");
-        t80.setText(res[8][0]);
-        if(isReflected(t[8][1])) t82.setText("TRUE");
-        t90.setText(res[9][0]);
-        if(isReflected(t[9][1])) t92.setText("TRUE");
-        t01.setText(res[0][1]);
+        if(isReflected(t[0][1])) {
+            l0.setText("TRUE");
+            l0.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[0][1])) {
+            p0.setStyle("-fx-background-color: red;");
+
+        }
+        t01.setText(res[1][0]);
+        if(isReflected(t[1][1])) {
+            l1.setText("TRUE");
+            l1.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[1][1]))  {
+            p1.setStyle("-fx-background-color: red;");
+
+        }
+        t02.setText(res[2][0]);
+        if(isReflected(t[2][1])) {
+            l2.setText("TRUE");
+            l2.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[2][1])) {
+            p2.setStyle("-fx-background-color: red;");
+
+        }
+        t03.setText(res[3][0]);
+        if(isReflected(t[3][1])){
+            l3.setText("TRUE");
+            l3.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[3][1])) {
+            p3.setStyle("-fx-background-color: red;");
+
+        }
+        t04.setText(res[4][0]);
+        if(isReflected(t[4][1])){
+            l4.setText("TRUE");
+            l4.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[4][1])){
+            p4.setStyle("-fx-background-color: red;");
+
+        }
+        t05.setText(res[5][0]);
+        if(isReflected(t[5][1])){
+            l5.setText("TRUE");
+            l5.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[5][1])){
+            p5.setStyle("-fx-background-color: red;");
+
+        }
+        t06.setText(res[6][0]);
+        if(isReflected(t[6][1])){
+            l6.setText("TRUE");
+            l6.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[6][1])) {
+            p6.setStyle("-fx-background-color: red;");
+
+        }
+        t07.setText(res[7][0]);
+        if(isReflected(t[7][1])){
+            l7.setText("TRUE");
+            l7.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[7][1])){
+            p7.setStyle("-fx-background-color: red;");
+
+        }
+        t08.setText(res[8][0]);
+        if(isReflected(t[8][1])){
+            l8.setText("TRUE");
+            l8.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[8][1])){
+            p8.setStyle("-fx-background-color: red;");
+
+        }
+        t09.setText(res[9][0]);
+        if(isReflected(t[9][1])){
+            l9.setText("TRUE");
+            l9.setStyle("-fx-text-fill: green");
+        }
+        if(isSumTen(res[9][1])){
+            p9.setStyle("-fx-background-color: red;");
+
+        }
+        t10.setText(res[0][1]);
         t11.setText(res[1][1]);
-        t21.setText(res[2][1]);
-        t31.setText(res[3][1]);
-        t41.setText(res[4][1]);
-        t51.setText(res[5][1]);
-        t61.setText(res[6][1]);
-        t71.setText(res[7][1]);
-        t81.setText(res[8][1]);
-        t91.setText(res[9][1]);
+        t12.setText(res[2][1]);
+        t13.setText(res[3][1]);
+        t14.setText(res[4][1]);
+        t15.setText(res[5][1]);
+        t16.setText(res[6][1]);
+        t17.setText(res[7][1]);
+        t18.setText(res[8][1]);
+        t19.setText(res[9][1]);
     }
 
 }
